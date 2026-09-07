7a603875d082620ed080a87a02dde8d9a8f9537e
Gabriel Dutra <gabrieldutra45@gmail.com>
2026-09-07 12:44:01 -0300

    fix url

--- canada-immigration-web/src/app/core/services/express-entry-draw.service.ts
+++ canada-immigration-web/src/app/core/services/express-entry-draw.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ExpressEntryDraw, PagedResult, PoolDistribution } from '../../models/express-entry-draw.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpressEntryDrawService {
-  private readonly baseUrl = 'http://localhost:5088/api/ExpressEntryDraws';
+  private readonly baseUrl = `${environment.apiBaseUrl}/ExpressEntryDraws`;
 
   constructor(private http: HttpClient) {}
 
--- canada-immigration-web/src/app/core/services/proof-of-funds.service.ts
+++ canada-immigration-web/src/app/core/services/proof-of-funds.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProofOfFundsResult } from '../../models/proof-of-funds.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProofOfFundsService {
-  private readonly baseUrl = 'http://localhost:5088/api/ProofOfFunds';
+  private readonly baseUrl = `${environment.apiBaseUrl}/ProofOfFunds`;
 
   constructor(private http: HttpClient) {}
 
